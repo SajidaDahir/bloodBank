7046524b0fe65f85256a22f47fa93bba9e1d3da1
Yaasir <yaasirmohammad397@gmail.com>
2025-11-17 18:37:27 +0300

    modified donor-response with session start

--- donor_response.php
+++ donor_response.php
@@ -9,7 +9,7 @@ $donor_id = (int)$_SESSION['donor_id'];
 $responses = [];
 try {
     $sql = "SELECT drr.created_at AS accepted_at,
-                   br.blood_type, br.units_needed, br.urgency, br.status AS request_status, br.created_at AS requested_at,
+                   br.request_type, br.blood_type, br.units_needed, br.urgency, br.deadline_at, br.status AS request_status, br.created_at AS requested_at,
                    h.hospital_name, h.city
             FROM donor_request_responses drr
             JOIN blood_requests br ON br.id = drr.request_id
@@ -22,41 +22,3 @@ try {
 } catch (Exception $e) {
     $responses = [];
 }
-
-$conf['page_title'] = 'My Responses | BloodBank';
-$Objlayout->header($conf);
-?>
-<?php $Objlayout->donorDashboardStart($conf,'responses'); ?>
-
-<div class="card">
-  <div class="card-title">My Accepted Responses</div>
-  <?php if (empty($responses)): ?>
-    <p>You haven't accepted any requests yet.</p>
-  <?php else: ?>
-    <div class="table-wrap">
-      <table class="table">
-        <thead>
-          <tr><th>Accepted</th><th>Hospital</th><th>City</th><th>Blood Type</th><th>Units</th><th>Urgency</th><th>Request Status</th><th>Requested</th></tr>
-        </thead>
-        <tbody>
-          <?php foreach($responses as $r): ?>
-            <tr>
-              <td><?php echo date('M j, Y', strtotime($r['accepted_at'])); ?></td>
-              <td><?php echo htmlspecialchars($r['hospital_name']); ?></td>
-              <td><?php echo htmlspecialchars($r['city']); ?></td>
-              <td><?php echo htmlspecialchars($r['blood_type']); ?></td>
-              <td><?php echo (int)$r['units_needed']; ?></td>
-              <td><?php echo htmlspecialchars($r['urgency']); ?></td>
-              <td><?php echo htmlspecialchars($r['request_status']); ?></td>
-              <td><?php echo date('M j, Y', strtotime($r['requested_at'])); ?></td>
-            </tr>
-          <?php endforeach; ?>
-        </tbody>
-      </table>
-    </div>
-  <?php endif; ?>
-</div>
-
-<?php $Objlayout->dashboardEnd(); ?>
-<?php $Objlayout->footer($conf); ?>
-
